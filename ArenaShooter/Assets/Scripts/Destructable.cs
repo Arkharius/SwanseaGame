7db44fffc7966647a5bff9d2d96b0df6af2ac675
Arkharius <nathan.visser@student-kmt.hku.nl>
2018-05-10 15:15:31 +0100

    *Fixed IDamageable *Added Destructable

---
+++ ArenaShooter/Assets/Scripts/Destructable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour, IDamageable {
+
+	public void TakeDamage(int damage)
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}
