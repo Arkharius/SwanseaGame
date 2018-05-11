3bb8620ce04e725904658b536ad16af36784b69d
Gwyn Perry <gmg@users.noreply.github.com>
2018-05-11 10:13:03 +0100

    Abstracted enemy base class

---
+++ ArenaShooter/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : EnemyBase {
+
+    private Transform m_target;
+
+
+    // Use this for initialization
+    void Start () {
+        m_target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        HandleMovement(m_target.position);
+    }
+
+}
---
+++ ArenaShooter/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour, IDamageable {
+
+    [SerializeField]
+    private Rigidbody2D m_rigidbody;
+
+    [SerializeField]
+    [Range(0.1f, 2.0f)]
+    private float m_turnRate = 0.5f;
+
+    [SerializeField]
+    private float m_movementSpeed;
+
+    [SerializeField]
+    private int m_hitPoints = 5;
+    
+
+    public void Die()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        m_hitPoints -= damage;
+
+        if (m_hitPoints <= 0)
+            Die();
+    }
+
+    protected void HandleMovement(Vector3 destination)
+    {
+        // track towards player
+        Vector2 targetVector = destination - transform.position;
+        float targetAngle = Vector2.SignedAngle(transform.up, targetVector);
+
+        // Turn towards target
+        m_rigidbody.AddTorque(targetAngle * m_turnRate);
+
+        // move towards target     
+        m_rigidbody.AddForce(transform.up * m_movementSpeed);
+    }
+}
